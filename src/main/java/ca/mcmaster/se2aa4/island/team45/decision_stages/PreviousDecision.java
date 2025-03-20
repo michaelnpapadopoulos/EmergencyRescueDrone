945ebb2b44c45be59857db89d5dd429a4e5f2ca1
Dhruv Chokshi <168044980+chokshidhruv@users.noreply.github.com>
2025-03-19 22:13:53 -0400

    Update PreviousDecision.java

--- src/main/java/ca/mcmaster/se2aa4/island/team45/decision_stages/PreviousDecision.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team45/decision_stages/PreviousDecision.java
@@ -1,30 +1,22 @@
 package ca.mcmaster.se2aa4.island.team45.decision_stages;
 
 public class PreviousDecision {
-    private String prevAction; // Prim obssesed
+    private String prevAction;
     private String prevHeading;
 
-    public PreviousDecision() {
-        this.prevAction = "";
-        this.prevHeading = "";
-    }
-
-    public void setPrevAction(String action) {
-        this.prevAction = action;
-    }
-
-    public void setPrevHeading(String heading) {
-        this.prevHeading = heading;
-    }
-
     public String getPrevAction() {
-        if (this.prevAction.equals("")) {
-            return null;
-        }
-        return this.prevAction;
+        return prevAction;
+    }
+
+    public void setPrevAction(String prevAction) {
+        this.prevAction = prevAction;
     }
 
     public String getPrevHeading() {
-        return this.prevHeading;
+        return prevHeading;
+    }
+
+    public void setPrevHeading(String prevHeading) {
+        this.prevHeading = prevHeading;
     }
 }

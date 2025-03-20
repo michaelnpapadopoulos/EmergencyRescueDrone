994d851a56022106e2763fa43602a3471d296b26
Maiku <91214380+Maikulol@users.noreply.github.com>
2025-03-20 17:28:59 -0400

    Change to coordinate system

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team45/map/CoordValue.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.se2aa4.island.team45.map;
+
+public class CoordValue {
+    int coord;
+
+    public CoordValue(int coord) {
+        this.coord = coord;
+    }
+
+    public int getCoordVal() {
+        return coord;
+    }
+}
--- src/main/java/ca/mcmaster/se2aa4/island/team45/map/CoordinateManager.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team45/map/CoordinateManager.java
@@ -4,29 +4,32 @@ import ca.mcmaster.se2aa4.island.team45.decision_stages.PreviousDecision;
 import ca.mcmaster.se2aa4.island.team45.drone.DirectionManager;
 
 public class CoordinateManager {
-    private int[] coordinates = new int[2];
+    private CoordValue[] coordinates = new CoordValue[2];
     private CoordinateAdjuster coordAdjuster = new CoordinateAdjuster();
 
 
     public CoordinateManager() {
-        this.coordinates[0] = 1;
-        this.coordinates[1] = 1;
+        this.coordinates[0] = new CoordValue(1);
+        this.coordinates[1] = new CoordValue(1);
     }
 
 
     public void adjustCoords(DirectionManager dm, PreviousDecision pd) {
-        this.coordinates = coordAdjuster.makeAdjustment(dm, pd, this.coordinates);
+        int[] originalCoords = new int[] {this.coordinates[0].getCoordVal(), this.coordinates[1].getCoordVal()};
+        int[] adjustedCoords = coordAdjuster.makeAdjustment(dm, pd, originalCoords);
+        this.coordinates[0] = new CoordValue(adjustedCoords[0]);
+        this.coordinates[1] = new CoordValue(adjustedCoords[1]);
     }
 
     public int[] getCoordinates() {
-        return this.coordinates;
+        return new int[] {this.coordinates[0].getCoordVal(), this.coordinates[1].getCoordVal()};
     }
 
     public int getX() {
-        return this.coordinates[0];
+        return this.coordinates[0].getCoordVal();
     }
 
     public int getY() {
-        return this.coordinates[1];
+        return this.coordinates[1].getCoordVal();
     }
 }

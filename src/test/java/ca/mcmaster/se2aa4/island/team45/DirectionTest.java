4e2258d7892f576fe8488080a38698844ce367ec
Dhruv Chokshi <168044980+chokshidhruv@users.noreply.github.com>
2025-03-21 15:42:58 -0400

    Create DirectionTest.java

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team45/DirectionTest.java
@@ -0,0 +1,28 @@
+package ca.mcmaster.se2aa4.island.team45;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import ca.mcmaster.se2aa4.island.team45.drone.direction.Direction;
+
+public class DirectionTest {
+
+    @Test
+    public void testUpdateDirection() {
+        Direction direction = new Direction("N");
+        assertEquals("N", direction.stringForward());
+        direction.updateDirection("E");
+        assertEquals("E", direction.stringForward());
+    }
+
+    @Test
+    public void testStringLeft() {
+        Direction direction = new Direction("N");
+        assertEquals("W", direction.stringLeft());
+    }
+
+    @Test
+    public void testStringRight() {
+        Direction direction = new Direction("N");
+        assertEquals("E", direction.stringRight());
+    }
+}

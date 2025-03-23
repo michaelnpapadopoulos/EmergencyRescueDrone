1e34d314343a7ef570e97833d8d67f27abbccdd4
Josh Engel <engelj4@mcmaster.ca>
2025-03-23 19:00:17 -0400

    Added getNearestCreekMethod

--- src/main/java/ca/mcmaster/se2aa4/island/team45/map/interest_points/POIManager.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team45/map/interest_points/POIManager.java
@@ -21,4 +21,24 @@ public class POIManager {
     public Site getSite() {
         return emergencySite;
     }
+
+    public Creek getNearestCreek() {
+        //Sets the first creek to the nearest
+        Creek nearestCreek = creeks.get(0);
+        int nearestX = creeks.get(0).getCreekCoordinate().getX() - emergencySite.getSiteCoordinate().getX();
+        int nearestY = creeks.get(0).getCreekCoordinate().getY() - emergencySite.getSiteCoordinate().getY();
+
+        //Checks if each following creek is closer if so sets it to the nreaest and continues until all creeks checked
+        for(int i = 1; i < creeks.size(); i++) {
+            int currentX = creeks.get(i).getCreekCoordinate().getX() - emergencySite.getSiteCoordinate().getX();
+            int currentY = creeks.get(i).getCreekCoordinate().getY() - emergencySite.getSiteCoordinate().getY();
+
+            if (Math.sqrt(Math.pow(currentX,2) + Math.pow(currentY,2)) < Math.sqrt(Math.pow(nearestX,2) + Math.pow(nearestY,2))) {
+                nearestCreek = creeks.get(i);
+                nearestX = currentX;
+                nearestY = currentY;
+            }
+        }
+        return nearestCreek;
+    }
 }

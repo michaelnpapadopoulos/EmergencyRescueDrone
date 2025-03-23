ea3a110210fcd928d4a26845dca5db1c76eb433b
Maiku <91214380+Maikulol@users.noreply.github.com>
2025-03-23 17:24:55 -0400

    Massive rebase
    Co-authored-by: Dhruv Chokshi <chokshidhruv@users.noreply.github.com>

--- src/main/java/ca/mcmaster/se2aa4/island/team45/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team45/Explorer.java
@@ -6,7 +6,6 @@ import org.apache.logging.log4j.Logger;
 import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import ca.mcmaster.se2aa4.island.team45.drone.FlightManager;
 
 public class Explorer implements IExplorerRaid {
 
@@ -16,31 +15,32 @@ public class Explorer implements IExplorerRaid {
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-
         logger.info("** Initialization info:\n {}",info.toString(2));
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
-        FlightManager.getInstance().passInitialInfo(direction, batteryLevel);
+
+
+
     }
 
     @Override
     public String takeDecision() {
-        String dec = FlightManager.getInstance().getDecision();
-        logger.info("Decision: {}", dec);
-        return dec;
+        
+        return null;
     }
 
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        FlightManager.getInstance().acknowledgeResults(response);
+        
+        return null;
     }
 
     @Override
     public String deliverFinalReport() {
-        return "no creek found";
+        return null;
     }
 
 }
---
+++ src/main/java/ca/mcmaster/se2aa4/island/team45/drone/battery/Battery.java
@@ -0,0 +1,6 @@
+package ca.mcmaster.se2aa4.island.team45.drone.battery;
+
+public class Battery {
+    private final int capacity;
+    private int charge; 
+}
